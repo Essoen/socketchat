fb343c23c07bfc5cdfcc630131603962ce3a7725
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-08-01 14:43:49 +0200

    Add Skeleton for simple styling. Move node_modules to root.

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ A simple chat application, written in Node with the help of [Socket.io](http://s
 
 ## Installation
     git clone git@github.com:Essoen/socketchat.git
-    cd socketchat/app
+    cd socketchat
     npm install
-    node app.js
     
-An the app is served at `localhost:3000`.
+Now run `node app.js` from the folder `app`, and the app is served at `localhost:3000`.
--- app/public/app.js
+++ app/public/app.js
@@ -2,26 +2,27 @@
  * Created by esso on 01.08.15.
  */
 'use strict';
-var socket = io();
-
-$('#send').click(function(e){
-    printMsg('Me', $('#new-message').val());
-    socket.emit('msg', {
-        user: $('#username').val(),
-        msg: $('#new-message').val()
+(function(){
+    var socket = io();
+    $('#send').click(function(e){
+        printMsg('Me', $('#new-message').val());
+        socket.emit('msg', {
+            user: $('#username').val(),
+            msg: $('#new-message').val()
+        });
+        $('#new-message').val(''); // Clear field
     });
-    $('#new-message').val(''); // Clear field
-});
 
-socket.on('msg', function(msgObj){
-    printMsg(msgObj.user, msgObj.msg);
-});
+    socket.on('msg', function(msgObj){
+        printMsg(msgObj.user, msgObj.msg);
+    });
 
-socket.on('ctrl', function(ctrlObj){
-    $('#messages').append(ctrlObj + '<br>')
-});
+    socket.on('ctrl', function(ctrlObj){
+        $('#messages').append(ctrlObj + '<br>')
+    });
 
-var printMsg = function(user, msg) {
-    $('#messages').append('<bold>' + user + ':</bold> ' + msg + '<br>')
+    var printMsg = function(user, msg) {
+        $('#messages').append('<bold>' + user + ':</bold> ' + msg + '<br>')
+    };
+}());
 
-};
