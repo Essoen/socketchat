83da4b085f20dbdf1b65ed657f2190c17b60edbd
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-07-24 18:59:18 +0200

    Remove disconneced users from being active

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@
  * Created by esso on 01.08.15.
  */
 'use strict';
+var _ = require('lodash');
+
 var express = require('express');
 var app = express();
 
@@ -11,12 +13,14 @@ var io = require('socket.io')(http);
 app.use(express.static('app/public')); // Serve webapp
 
 var usernames = [];
+var clients = [];
 io.on('connection', function(socket){
     socket.on('login', function(userObj){
         socket.broadcast.emit('ctrl', {
             msg: userObj.username + ' logged in'
         });
         usernames.push(userObj.username);
+        clients.push({socket: socket, username: userObj.username});
         io.emit('users', usernames);
     });
 
@@ -32,8 +36,10 @@ io.on('connection', function(socket){
 
     socket.on('disconnect', function(){
         // Broadcast usernames again, the case the user did not log out
+        var client = findClient(socket);
+        socket.broadcast.emit('ctrl', {msg: client.username + ' disconnected.'});
+        socketDisconnected(socket);
         socket.broadcast.emit('users', usernames);
-        // @TODO broadcast message about user leaving in the case he did not log out first
     });
 });
 
@@ -41,6 +47,23 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
     console.log('listening on port 3000');
 });
+
+var findClient = function(socket){
+    for (var i = 0; i < clients.length; i++){
+        if (socket === clients[i].socket) return clients[i];
+    }
+    return false;
+}
+
+var socketDisconnected = function(socket){
+    var client = findClient(socket);
+    if(client){
+        removeUsername(client.username)
+        _.remove(clients, function(e) {
+            return e.socket === socket;
+        });
+    }
+}
 
 var removeUsername = function(name){
     var i = usernames.indexOf(name);
